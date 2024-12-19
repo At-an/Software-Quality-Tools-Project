65ae2913ed8df6b6c34b4582ac475109bb05ebae
atankeutchakoute@gmail.com <atankeutchakoute@gmail.com>
2024-12-19 10:15:21 +0100

    Style tests and webbsite directory content according to flake8

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,41 +1,84 @@
-def test_home_page(client, create_user):  
-    user = create_user(email='test@example.com', username='testuser', password='password123')
-    response = client.post('/login', data={'email': 'test@example.com', 'password': 'password123'}, follow_redirects=True)
+def test_home_page(client, create_user):
+    """Test home page."""
+    user = create_user(
+        email="test@example.com", username="testuser", password="password123"
+    )
+    response = client.post(
+        "/login",
+        data={"email": "test@example.com", "password": "password123"},
+        follow_redirects=True,
+    )
     assert response.status_code == 200
-    response = client.get('/', follow_redirects=True)
+    response = client.get("/", follow_redirects=True)
     assert response.status_code == 200  # Ensure no redirect
 
+
 def test_create_post(client, create_user):
-    user = create_user(email='test@example.com', username='testuser', password='password123')
-    response = client.post('/login', data={'email': 'test@example.com', 'password': 'password123'}, follow_redirects=True)
+    """Test create post."""
+    user = create_user(
+        email="test@example.com", username="testuser", password="password123"
+    )
+    response = client.post(
+        "/login",
+        data={"email": "test@example.com", "password": "password123"},
+        follow_redirects=True,
+    )
     assert response.status_code == 200
-    response = client.post('/create-post', data={'text': 'My first post!'}, follow_redirects=True)
+    response = client.post(
+        "/create-post", data={"text": "My first post!"}, follow_redirects=True
+    )
     assert response.status_code == 200
+
 
 def test_create_post_validation(client, create_user):
-    user = create_user(email='test@example.com', username='testuser', password='password123')
-    client.post('/login', data={'email': 'test@example.com', 'password': 'password123'})
-    response = client.post('/create-post', data={'text': ''})
-    long_text = 'a' * 1001
-    response = client.post('/create-post', data={'text':long_text})
+    """Test create post validation."""
+    user = create_user(
+        email="test@example.com", username="testuser", password="password123"
+    )
+    client.post(
+        "/login", data={"email": "test@example.com", "password": "password123"}
+    )
+    response = client.post("/create-post", data={"text": ""})
+    long_text = "a" * 1001
+    response = client.post("/create-post", data={"text": long_text})
+
 
 def test_delete_post(client, create_user):
-    user = create_user(email='test@example.com', username='testuser', password='password123')
-    client.post('/login', data={'email': 'test@example.com', 'password': 'password123'})
-    client.post('/create-post', data={'text': 'My first post!'})
-    response = client.get('/delete-post/1')
+    """Test delete post."""
+    user = create_user(
+        email="test@example.com", username="testuser", password="password123"
+    )
+    client.post(
+        "/login", data={"email": "test@example.com", "password": "password123"}
+    )
+    client.post("/create-post", data={"text": "My first post!"})
+    response = client.get("/delete-post/1")
     assert response.status_code == 302  # Redirect to home page
+
 
 def test_create_comment(client, create_user):
-    user = create_user(email='test@example.com', username='testuser', password='password123')
-    client.post('/login', data={'email': 'test@example.com', 'password': 'password123'})
-    client.post('/create-post', data={'text': 'My first post!'})
-    response = client.post('/create-comment/1', data={'text': 'Nice post!'})
+    """Test create comment."""
+    user = create_user(
+        email="test@example.com", username="testuser", password="password123"
+    )
+    client.post(
+        "/login", data={"email": "test@example.com", "password": "password123"}
+    )
+    client.post("/create-post", data={"text": "My first post!"})
+    response = client.post("/create-comment/1", data={"text": "Nice post!"})
     assert response.status_code == 302  # Redirect to home page
 
+
 def test_like_post(client, create_user):
-    user = create_user(email='test@example.com', username='testuser', password='password123')
-    response = client.post('/login', data={'email': 'test@example.com', 'password': 'password123'}, follow_redirects=True)
+    """Test like post."""
+    user = create_user(
+        email="test@example.com", username="testuser", password="password123"
+    )
+    response = client.post(
+        "/login",
+        data={"email": "test@example.com", "password": "password123"},
+        follow_redirects=True,
+    )
     assert response.status_code == 200
-    response = client.post('/like-post/1', follow_redirects=True)
+    response = client.post("/like-post/1", follow_redirects=True)
     assert response.status_code == 200
